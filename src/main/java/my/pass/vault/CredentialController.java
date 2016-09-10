a1c1af0915cc83d52a80e5c565b7659df6e6a023
kaunjovi <kaunjovi@gmail.com>
2016-09-11 00:09:31 +0530

    Now we have a collection of credentials coming out.

--- src/main/java/my/pass/vault/CredentialController.java
+++ src/main/java/my/pass/vault/CredentialController.java
@@ -1,5 +1,7 @@
 package my.pass.vault;
 
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,15 +9,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class CredentialController {
 
 	@RequestMapping("/credential")
-	public Credential dummyMethodForTesting(){
+	public ArrayList<Credential> dummyMethodForTesting(){
+		
+		ArrayList<Credential> credentialCollection = new ArrayList<Credential>() ; 
 		
 		Credential dummyCredential = new Credential() ; 
 		dummyCredential.setName("Google");
 		dummyCredential.setPath("http://www.google.com");
 		dummyCredential.setUserName("kaunjovi");
 		dummyCredential.setPassword("goodtry");
+		credentialCollection.add(dummyCredential) ; 
+
+		Credential dummyCredential1 = new Credential() ; 
+		dummyCredential1.setName("Google1");
+		dummyCredential1.setPath("http://www.google.com1");
+		dummyCredential1.setUserName("kaunjovi1");
+		dummyCredential1.setPassword("goodtry1");
+		credentialCollection.add(dummyCredential1) ; 
 		
-		return dummyCredential ; 
+		return credentialCollection ; 
 		
 	}
 }
